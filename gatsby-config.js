f24e66061db048387fd09fc27cc1d73a1b37a76c
Ismaila Abdoulahi <code.ismaila@gmail.com>
2020-12-18 16:42:14 +0100

    Update configs
    Hand siteMetadata management to cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,8 @@
+const siteMetadata = require('./content/settings/sitemetadata/index.json');
+
 module.exports = {
   siteMetadata: {
-    title: `Open Knowledge Belgium`,
-    // TODO UPDATE FOLLOWING
-    author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`
-    },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
-    social: {
-      twitter: `kylemathews`
-    }
+    ...siteMetadata
   },
   plugins: [
     {
